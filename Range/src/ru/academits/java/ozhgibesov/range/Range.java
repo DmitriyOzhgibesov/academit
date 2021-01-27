a85f9e7659bb6fce6d7f5fba4194d7a79220a160
DmitriyOzhgibesov <dozhgibesov@gmail.com>
2021-01-27 21:13:44 +0700

    Реализация сложной версии решения задачи "Range": 1. Добавлены методы доступа getTo(), setTo; 2. Добавлены методы для получения пересечения, объединения, вычитания диапазонов.

--- Range/src/ru/academits/java/ozhgibesov/range/Range.java
+++ Range/src/ru/academits/java/ozhgibesov/range/Range.java
@@ -12,6 +12,14 @@ public class Range {
         this.from = from;
     }
 
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
     public Range(double from, double to) {
         this.from = from;
         this.to = to;
@@ -23,5 +31,59 @@ public class Range {
 
     public boolean isInside(double number) {
         return number >= from && number <= to;
+    }
+
+    public Range[] getUnion(Range otherRange) {
+        Range[] arrayRange = new Range[]{null};
+
+        if (this.getFrom() <= otherRange.getFrom() && this.getTo() <= otherRange.getTo() && otherRange.getFrom() <= this.getTo()) {
+            arrayRange = new Range[]{new Range(this.getFrom(), otherRange.getTo())};
+        } else if (otherRange.getFrom() <= this.getFrom() && otherRange.getTo() <= this.getTo() && this.getFrom() <= otherRange.getTo()) {
+            arrayRange = new Range[]{new Range(otherRange.getFrom(), this.getTo())};
+        } else if (this.getFrom() <= otherRange.getFrom() && this.getTo() >= otherRange.getTo()) {
+            arrayRange = new Range[]{new Range(this.getFrom(), this.getTo())};
+        } else if (otherRange.getFrom() <= this.getFrom() && otherRange.getTo() >= this.getTo()) {
+            arrayRange = new Range[]{new Range(otherRange.getFrom(), otherRange.getTo())};
+        } else if (this.getFrom() <= otherRange.getFrom() && this.getTo() <= otherRange.getTo() && otherRange.getFrom() > this.getTo()) {
+            arrayRange = new Range[2];
+            arrayRange[0] = new Range(this.getFrom(), this.getTo());
+            arrayRange[1] = new Range(otherRange.getFrom(), otherRange.getTo());
+        } else if (otherRange.getFrom() <= this.getFrom() && otherRange.getTo() <= this.getTo() && this.getFrom() > otherRange.getTo()) {
+            arrayRange = new Range[2];
+            arrayRange[0] = new Range(otherRange.getFrom(), otherRange.getTo());
+            arrayRange[1] = new Range(this.getFrom(), this.getTo());
+        }
+
+        return arrayRange;
+    }
+
+    public Range getIntersection(Range otherRange) {
+        if (this.getFrom() <= otherRange.getFrom() && this.getTo() < otherRange.getTo() && otherRange.getFrom() < this.getTo()) {
+            return new Range(otherRange.getFrom(), this.getTo());
+        } else if (otherRange.getFrom() <= this.getFrom() && this.getTo() > otherRange.getTo() && this.getFrom() < otherRange.getTo()) {
+            return new Range(this.getFrom(), otherRange.getTo());
+        } else if (this.getFrom() <= otherRange.getFrom() && otherRange.getTo() <= this.getTo()) {
+            return new Range(otherRange.getFrom(), otherRange.getTo());
+        } else if (otherRange.getFrom() <= this.getFrom() && this.getTo() <= otherRange.getTo()) {
+            return new Range(this.getFrom(), this.getTo());
+        }
+
+        return null;
+    }
+
+    public Range[] getSubtraction(Range otherRange) {
+        Range[] arrayRange = new Range[]{null};
+
+        if (this.getFrom() < otherRange.getFrom() && this.getTo() < otherRange.getTo() && otherRange.getFrom() < this.getTo()) {
+            arrayRange = new Range[]{new Range(this.getFrom(), otherRange.getFrom())};
+        } else if (otherRange.getFrom() < this.getFrom() && otherRange.getTo() < this.getTo() && this.getFrom() < otherRange.getTo()) {
+            arrayRange = new Range[]{new Range(otherRange.getTo(), this.getTo())};
+        } else if (this.getFrom() < otherRange.getFrom() && this.getTo() > otherRange.getTo()) {
+            arrayRange = new Range[2];
+            arrayRange[0] = new Range(this.getFrom(), otherRange.getFrom());
+            arrayRange[1] = new Range(otherRange.getTo(), this.getTo());
+        }
+
+        return arrayRange;
     }
 }
--- Range/src/ru/academits/java/ozhgibesov/range/RangeMain.java
+++ Range/src/ru/academits/java/ozhgibesov/range/RangeMain.java
@@ -5,23 +5,69 @@ import java.util.Scanner;
 public class RangeMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите начальную границу диапазона: ");
-        double startRange = scanner.nextDouble();
+        System.out.print("Введите начальную границу диапазона 1: ");
+        double startRange1 = scanner.nextDouble();
 
-        System.out.print("Введите конечную границу диапазона: ");
-        double endRange = scanner.nextDouble();
+        System.out.print("Введите конечную границу диапазона 1: ");
+        double endRange1 = scanner.nextDouble();
 
-        System.out.print("Введите число для проверки входимости в диапазон: ");
+        System.out.print("Введите число для проверки входимости в диапазон 1: ");
         double number = scanner.nextDouble();
 
-        Range range1 = new Range(startRange, endRange);
+        Range range1 = new Range(startRange1, endRange1);
 
         System.out.println("Длина диапазона составляет " + range1.getLength());
 
         if (range1.isInside(number)) {
-            System.out.printf("Число %.2f входит в диапазон между %.2f и %.2f", number, startRange, endRange);
+            System.out.printf("Число %.2f входит в диапазон между %.2f и %.2f\n", number, startRange1, endRange1);
         } else {
-            System.out.printf("Число %.2f не входит в диапазон между %.2f и %.2f", number, startRange, endRange);
+            System.out.printf("Число %.2f не входит в диапазон между %.2f и %.2f\n", number, startRange1, endRange1);
         }
+
+        System.out.print("Введите начальную границу диапазона 2: ");
+        double startRange2 = scanner.nextDouble();
+
+        System.out.print("Введите конечную границу диапазона 2: ");
+        double endRange2 = scanner.nextDouble();
+
+        Range range2 = new Range(startRange2, endRange2);
+
+        Range[] unitedRange;
+        unitedRange = range1.getUnion(range2);
+
+        System.out.printf("Диапазон 1 = [%.2f;%.2f]\n", range1.getFrom(), range1.getTo());
+        System.out.printf("Диапазон 2 = [%.2f;%.2f]\n", range2.getFrom(), range2.getTo());
+
+        StringBuilder unionResult = new StringBuilder("Результат объединения =");
+        for (Range range : unitedRange) {
+            if (range != null) {
+                unionResult.append(String.format(" [%.2f;%.2f]", range.getFrom(), range.getTo()));
+            } else {
+                unionResult.append(" [null]");
+            }
+        }
+
+        System.out.println(unionResult);
+
+        Range intersectionRange = range1.getIntersection(range2);
+        if (intersectionRange != null) {
+            System.out.printf("Результат пересечения: [%.2f;%.2f]\n", intersectionRange.getFrom(), intersectionRange.getTo());
+        } else {
+            System.out.println("Пересечение отсутствует");
+        }
+
+        Range[] subtractedRange;
+        subtractedRange = range1.getSubtraction(range2);
+
+        StringBuilder subtractedResult = new StringBuilder("Результат вычитания =");
+        for (Range range : subtractedRange) {
+            if (range != null) {
+                subtractedResult.append(String.format(" [%.2f;%.2f]", range.getFrom(), range.getTo()));
+            } else {
+                subtractedResult.append(" [null]");
+            }
+        }
+
+        System.out.println(subtractedResult);
     }
 }
